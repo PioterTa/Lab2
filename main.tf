0c7db36940ad2336ae78c8f7bfa19acad2a98d6e
PioterTa <152792447+PioterTa@users.noreply.github.com>
2024-05-26 19:17:10 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
 provider "aws" {
-  region = "us-east-1"
-  assume_role {
-    role_arn = "ARN_roli_IAM"
-  }
+  region     = "us-east-1"
+  access_key = "FAKE_ACCESS_KEY"
+  secret_key = "FAKE_SECRET_KEY"
+  endpoint   = "http://localhost:4566"  # LocalStack endpoint
 }
 
 resource "aws_s3_bucket" "my_bucket" {
