e67ed559fb92adc7fddb29e75e3d6a78355d7296
PioterTa <152792447+PioterTa@users.noreply.github.com>
2024-05-26 19:33:10 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,4 @@
-provider "aws" {
-  region                  = "us-east-1"
-  access_key              = "FAKE_ACCESS_KEY"
-  secret_key              = "FAKE_SECRET_KEY"
-  s3_force_path_style     = true
-  skip_credentials_validation = true
-  skip_metadata_api_check = true
+resource "aws_s3_bucket" "example_bucket" {
+  bucket = var.bucket_name
+  acl    = "private"
 }
