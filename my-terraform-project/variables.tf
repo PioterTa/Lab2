3e990fb1e47b4dd3ffcd1f08dbf27a24fc4db254
PioterTa <152792447+PioterTa@users.noreply.github.com>
2024-05-26 18:59:24 +0200

    Create variables.tf

---
+++ my-terraform-project/variables.tf
@@ -0,0 +1,21 @@
+variable "aws_access_key" {
+  description = "AWS access key"
+  type        = string
+}
+
+variable "aws_secret_key" {
+  description = "AWS secret key"
+  type        = string
+}
+
+variable "region" {
+  description = "AWS region"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "bucket_name" {
+  description = "Name of the S3 bucket"
+  type        = string
+  default     = "my-test-bucket"
+}
