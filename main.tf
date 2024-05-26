f312d08ee919ac51407d1b3984c04a6a11a359cd
PioterTa <152792447+PioterTa@users.noreply.github.com>
2024-05-26 19:19:19 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,8 @@
 provider "aws" {
-  region     = "us-east-1"
-  access_key = "FAKE_ACCESS_KEY"
-  secret_key = "FAKE_SECRET_KEY"
-  endpoint   = "http://localhost:4566"  # LocalStack endpoint
-}
-
-resource "aws_s3_bucket" "my_bucket" {
-  bucket = "my-test-bucket"
-  acl    = "private"
+  region                  = "us-east-1"
+  access_key              = "FAKE_ACCESS_KEY"
+  secret_key              = "FAKE_SECRET_KEY"
+  s3_force_path_style     = true
+  skip_credentials_validation = true
+  skip_metadata_api_check = true
 }
