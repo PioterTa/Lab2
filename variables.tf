c21aad213c1112f69efe9150c6a62a74308885f4
PioterTa <152792447+PioterTa@users.noreply.github.com>
2024-05-26 19:33:24 +0200

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,5 +1,5 @@
-variable "region" {
-  description = "AWS region"
+variable "bucket_name" {
+  description = "The name of the S3 bucket"
   type        = string
-  default     = "us-east-1"
+  default     = "my-localstack-bucket"
 }
