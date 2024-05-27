bb1aff335290ec55c1422d64861e3e5c0f4e2669
PioterTa <152792447+PioterTa@users.noreply.github.com>
2024-05-27 15:22:05 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,10 @@
-resource "aws_s3_bucket" "example_bucket" {
+provider "aws" {
+  access_key = "test"
+  secret_key = "test"
+  region     = "us-east-1"
+  endpoint   = "http://localhost:4566"
+}
+
+resource "aws_s3_bucket" "my_bucket" {
   bucket = var.bucket_name
-  acl    = "private"
 }
