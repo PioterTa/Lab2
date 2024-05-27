146ee5fe918677029a94c11981557545a392e129
PioterTa <152792447+PioterTa@users.noreply.github.com>
2024-05-27 15:22:35 +0200

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,4 @@
-output "bucket_name" {
-  description = "Name of the created S3 bucket"
-  value       = aws_s3_bucket.my_bucket.bucket
-}
-
-output "bucket_arn" {
-  description = "ARN of the created S3 bucket"
-  value       = aws_s3_bucket.my_bucket.arn
+output "bucket_id" {
+  description = "The ID of the S3 bucket"
+  value       = aws_s3_bucket.my_bucket.id
 }
