7af48f509b1d096a4a0e72acb7f59c020ab1f2df
PioterTa <152792447+PioterTa@users.noreply.github.com>
2024-05-27 15:35:38 +0200

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,7 +1,7 @@
 provider "aws" {
   region                      = "us-east-1"
-  access_key                  = "AKIA4MTWKJONBDCKB75V"
-  secret_access_key            = "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"
+  access_key                  = "mock_access_key"
+  secret_key                  = "mock_secret_key"
   s3_force_path_style         = true
   skip_credentials_validation = true
   skip_metadata_api_check     = true
